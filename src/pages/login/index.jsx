2fe85541f24eb72689bec5d0aba4cc592a8833cb
Okutubo Lekan Michael <86299143+olakved@users.noreply.github.com>
2023-05-08 21:23:38 +0100

    corrected

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -100,7 +100,6 @@ function LoginPage() {
 
             <button
               type="submit"
-              onClick={toast.success("working", toastObject)}
               className="h-[40px] rounded-lg bg-white text-darky-col cursor-pointer"
             >
               Login

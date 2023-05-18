cef76caf9a5bf83141d6e1349c1a09aecfd807f2
Okutubo Lekan Michael <86299143+olakved@users.noreply.github.com>
2023-05-18 01:25:50 +0100

    loader

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -57,9 +57,9 @@ function BlogPage() {
         <div className="flex gap-4">
           <p className="animate-ping rounded-full h-4 w-4 bg-light-green font-extrabold  text-[30px]"></p>
           {/* <p className="font-bold ">loading...</p> */}
-          <p className="absolute top-[50%] bottom-[50%] left-[50%] right-[50%]">
+          <div className="absolute top-[50%] bottom-[50%] left-[50%] right-[50%]">
             <Spinner />
-          </p>
+          </div>
         </div>
       ) : error ? (
         <p>Error while fetching blog data.</p>

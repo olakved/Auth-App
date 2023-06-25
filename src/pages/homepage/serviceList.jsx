0829a046fa89ce2336164dcb7890c7586f0e5c41
Okutubo Lekan Michael <86299143+olakved@users.noreply.github.com>
2023-06-25 14:17:01 +0100

    update

--- src/pages/homepage/serviceList.jsx
+++ src/pages/homepage/serviceList.jsx
@@ -3,7 +3,14 @@ import imgA from "../../assets/profileImg.jpg";
 import imgB from "../../assets/profileImg2.jpg";
 import imgC from "../../assets/salesman.jpeg";
 import { GrFormPrevious, GrFormNext } from "react-icons/gr";
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import {
+  AiOutlineShoppingCart,
+  AiFillGithub,
+  AiOutlineBgColors,
+  AiOutlineCreditCard,
+  AiOutlineCodeSandbox,
+  AiOutlineReconciliation,
+} from "react-icons/ai";
 
 const serviceName = ["Mariam", "Motun", "Ahmed", "lekan", "Korede"];
 
@@ -59,30 +66,60 @@ const array3 = [
 
 const cards = [
   {
-    icon: <AiOutlineShoppingCart />,
+    icon: <AiFillGithub />,
     title: "Digital Design",
     subTitle:
       "Some quick example text to build on the card title and make up the bulk of the card's content. Moltin gives you the platform.",
   },
   {
-    icon: <AiOutlineShoppingCart />,
+    icon: <AiOutlineBgColors />,
     title: "Unlimited Colors",
     subTitle:
       "Credibly brand standards compliant users without extensible services. Anibh euismod tincidunt ut laoreet.",
   },
   {
-    icon: <AiOutlineShoppingCart />,
+    icon: <AiOutlineReconciliation />,
     title: "Strategy Solutions",
     subTitle:
       "Some quick example text to build on the card title and make up the bulk of the card's content. Moltin gives you the platform.",
+  },
+  {
+    icon: <AiOutlineCodeSandbox />,
+    title: "Awesome Support",
+    subTitle:
+      "Some quick example text to build on the card title and make up the bulk of the card's content. Moltin gives you the platform.",
+  },
+  {
+    icon: <AiOutlineShoppingCart />,
+    title: "Truly Multipurpose",
+    subTitle:
+      "It is a paradisematic country, in which roasted parts of sentences fly into your mouth leave for the far World.",
+  },
+  {
+    icon: <AiOutlineCreditCard />,
+    title: "Easy to customize",
+    subTitle:
+      "Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia.",
   },
 ];
 
 function ServiceList() {
   return (
     <div>
-      {/* <p className="mt-[150px] text-red-500">Card Array</p> */}
-      <div className="flex sm:flex-wrap justify-center gap-[20px] px-[50px] md:px-[10px] sm:px-[30px] py-[30px] mb-[100px]">
+      <div className="flex justify-center mb-[50px]">
+        <div className="flex flex-col items-center max-w-[700px] text-center">
+          <h1 className="text-[40px] sm:text-[25px] font-[500] mb-[20px]">
+            OUR SERVICES
+          </h1>
+          <p className="h-[4px] w-[100px] bg-light-col"></p>
+          <p className="text-[15px] leading-[24px] mt-[25px]">
+            We craft digital, graphic and dimensional thinking, to create
+            category leading brand experiences that have meaning and add a value
+            for our clients.
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-x-[20px] gap-y-[40px] px-[50px] md:px-[10px] sm:px-[30px] py-[30px] mb-[100px]">
         {cards.map((item, i) => (
           <div
             key={i}
